b8a5935e8c770510dd36cbb8b96411f539836d9d
Tal Leming <tal@typesupply.com>
2015-04-06 09:00:34 -0400

    Significant refactoring and upgrade.
    - refactored almost everything
    - added more data extraction
    - built infrastructure for multi-environment running
    - updated to do list

--- fontSizeReporter/fontSizeReporter.py
+++ fontSizeReporter/fontSizeReporter.py
@@ -5,23 +5,35 @@ and sub-directories. The profile contains hashes of
 the file contents and the file name as a way of
 filtering out duplicate data in the overall data set.
 
-To Do:
-- if defcon is installed, read UFOs with that.
-- if extractor is installed, use it to pull kerning from binaries.
-- should component transformations be extracted?
-- should anchors be extracted?
-- extract kerning pairs and kerning groups when possible.
-- in UFOs, sniff the layer glyph counts from the RoboFont lib location?
-- in UFOs, measure the number of characters in the features.
-- in UFOs, count the number of points with identifiers.
-- count the number of smooth curve points.
-- measure string lengths in font info and note them as an overall total.
-  this can then be split or stored in a single field in the generated UFOs.
-- should glyph name lengths be measured?
-- should point identifiers be assigned? these can potentially be a lot of data.
-- measure feature string length.
-- lib should be ignored.
-- measure glyph note lengths and store that as a single value.
+To Do
+-----
+
+General:
+- measure font info string length
+- store anchor count
+- measure glyph note lengths
+- count points with names/identifiers
+- count smooth points
+- measure glyph name lengths
+- output component data
+- output group data
+- output feature data
+- the commandline functions are clumsy
+- build a vanilla interface for use in RoboFont and Glyphs
+- write documentation
+
+OTF:
+
+defcon:
+
+RoboFab:
+- implement
+
+RoboFont:
+- implement
+
+Glyphs:
+- implement
 """
 
 import md5
@@ -30,78 +42,172 @@ import optparse
 from fontTools.ttLib import TTFont
 from fontTools.pens.basePen import BasePen
 
-# ----
-# Main
-# ----
+# ---------------------
+# Environment Detection
+# ---------------------
 
-def dumpProfilesForFonts(paths):
-	for path in paths:
-		profileFont(path)
+haveExtractor = False
+haveDefcon = False
+haveRoboFab = False
+haveRoboFont = False
+haveGlyphsApp = False
 
-# --------------
-# File Gathering
-# --------------
+try:
+	import extractor
+	haveExtractor = True
+except ImportError:
+	pass
 
-def gatherFontPaths(paths):
-	found = []
-	for path in paths:
-		if os.path.isdir(path):
-			for fileName in os.listdir(path):
-				if fileName.startswith("."):
-					continue
-				p = os.path.join(path, fileName)
-				found += gatherFontPaths([p])
-		else:
-			if isFontPath(path):
-				found.append(path)
-	return found
+try:
+	import defcon
+	haveDefcon = True
+except ImportError:
+	pass
 
-def isFontPath(path):
-	fileName = os.path.basename(path)
-	suffix = os.path.splitext(fileName)[-1].lower()
-	return suffix in (".otf", ".ttf")
+try:
+	import robofab
+	haveRoboFab = True
+except ImportError:
+	pass
 
-# --------
-# Profiler
-# --------
+try:
+	import mojo
+	haveRoboFont = True
+except ImportError:
+	pass
+
+# try:
+# 	import ?
+# 	haveGlyphsApp = True
+# except ImportError:
+# 	pass
+
+# -------------
+# Profile: Font
+# -------------
 
 def profileFont(path):
 	"""
 	Profile a font and dump the result to stdout.
 	"""
 	profile = dict(
+		# environment data
+		outputEnvironment="Unknown",
+		sourceFormat="Unknown",
+		# mapping of (x, y) : count for entire font.
+		# will be converted to a hash later.
 		fingerprinter={},
-		glyphs=0,
+		# number of glyphs per layer
+		glyphs=[],
+		# mapping of contour counts : number of glyphs with this number of contours
 		contourOccurance={},
+		# mapping of point count : number of contours with this point count
 		pointOccurance={},
-		componentOccurance=0,
+		# overall point type counts
 		pointTypes=dict(
 			moveTo=0,
 			lineTo=0,
 			curveTo=0,
 			qCurveTo=0
-		)
+		),
+		# component transformations
+		componentOccurance={},
+		# kerning pairs
+		kerningPairs=0,
+		# groups
+		groups={},
+		# characters in features
+		features=0,
+		# characters in all font info strings
+		fontInfo=0,
 	)
-	# counting
-	font = TTFont(path)
-	glyphOrder = font.getGlyphOrder()
-	glyphSet = font.getGlyphSet()
-	for glyphName in glyphOrder:
+	# send to format specific profilers
+	ext = os.path.splitext(path)[-1].lower()
+	profile["sourceFormat"] = ext
+	if ext in (".otf", ".ttf"):
+		_profileFont_OTF(path, profile)
+	elif ext == ".ufo" and (haveRoboFont or haveDefcon or haveRoboFab):
+		if haveRoboFont:
+			_profileFont_RoboFont(path, profile)
+	elif ext == ".glyphs" and haveGlyphsApp:
+		_profileFont_Glyphs(path, profile)
+	else:
+		return
+	# fingerprint
+	fingerprintProfile(profile)
+	# done
+	return profile
+
+def _profileFont_OTF(path, profile):
+	if haveExtractor and (haveDefcon or haveRoboFab):
+		if haveDefcon:
+			font = defcon.Font()
+		elif haveRoboFab:
+			font = robofab.objects.objectsRF.RFont()
+		extractor.extractUFO(path, font)
+		if haveDefcon:
+			_profileFont_defcon(font, profile)
+		elif haveRoboFab:
+			_profileFont_RoboFab(font, profile)
+	else:
+		profile["outputEnvironment"] = "fontTools"
+		font = TTFont(path)
+		glyphNames = font.getGlyphOrder()
+		glyphSet = font.getGlyphSet()		
+		profileGlyphSet(glyphNames, glyphSet)
+
+def _profileFont_RoboFont(path, profile):
+	"""
+	RoboFont specific profiler.
+	"""
+	profile["outputEnvironment"] = "RoboFont"
+	raise NotImplementedError
+
+def _profileFont_Glyphs(path, profile):
+	"""
+	Glyphs specific profiler.
+	"""
+	profile["outputEnvironment"] = "Glyphs"
+	raise NotImplementedError
+
+def _profileFont_defcon(path, profile):
+	"""
+	defcon specific profiler.
+	"""
+	profile["outputEnvironment"] = "defcon"
+	if isinstance(path, defcon.Font):
+		font = path
+	else:
+		font = defcon.Font(path)
+	try:
+		for layer in font.layers:
+			profileGlyphSet(layer.keys(), layer, profile)
+	except AttributeError:
+		profileGlyphSet(font.keys(), font, profile)
+	profileKerning(font.kerning, profile)
+	profileGroups(font.groups, profile)
+	profileFeatures(font.features.text, profile)
+	profileFontInfo(font.info, profile)
+
+def _profileFont_RoboFab(path, profile):
+	"""
+	RoboFab specific profiler.
+	"""
+	profile["outputEnvironment"] = "RoboFab"
+	raise NotImplementedError
+
+# ---------------
+# Profile: Glyphs
+# ---------------
+
+def profileGlyphSet(glyphNames, glyphSet, profile):
+	"""
+	Profile all glyphs in a glyph set.
+	"""
+	profile["glyphs"].append(len(glyphNames))
+	for glyphName in glyphNames:
 		glyph = glyphSet[glyphName]
 		profileGlyph(glyph, profile)
-	profile["glyphs"] = len(glyphOrder)
-	fingerprintProfile(profile)
-	# dump
-	print "> %d Glyphs (%s)" % (profile["glyphs"], profile["fingerprint"])
-	print "fingerprint:", profile["fingerprint"]
-	print "glyphs:", profile["glyphs"]
-	for contourCount, occurance in reversed(sorted(profile["contourOccurance"].items())):
-		print "glyphs with %d contours:" % contourCount, occurance
-	for pointCount, occurance in reversed(sorted(profile["pointOccurance"].items())):
-		print "contours with %d points:" % pointCount, occurance
-	for pointType in ("moveTo", "lineTo", "curveTo", "qCurveTo"):
-		print "%s points:" % pointType, profile["pointTypes"][pointType]
-	print "<"
 
 def profileGlyph(glyph, profile):
 	"""
@@ -128,7 +234,10 @@ def profileGlyph(glyph, profile):
 		profile["pointTypes"]["curveTo"] += contour["curveTo"]
 		profile["pointTypes"]["qCurveTo"] += contour["qCurveTo"]
 	# component counts
-	profile["componentOccurance"] += pen.components
+	for transformation in pen.components:
+		if transformation not in profile["componentOccurance"]:
+			profile["componentOccurance"][transformation] = 0
+		profile["componentOccurance"][transformation] += 1
 
 def fingerprintProfile(profile):
 	"""
@@ -156,7 +265,7 @@ class ProfilePen(BasePen):
 
 	def __init__(self, fingerprinter):
 		self.contours = []
-		self.components = 0
+		self.components = []
 		self._fingerprinter = fingerprinter
 
 	def _logPoint(self, pt):
@@ -194,11 +303,103 @@ class ProfilePen(BasePen):
 		self._logPoint(pt2)
 
 	def addComponent(self, glyphName, transformation):
-		self.components += 1
+		self.components.append(transformation)
 
-# --------------------
-# Command Line Behvior
-# --------------------
+# ----------------
+# Profile: Kerning
+# ----------------
+
+def profileKerning(kerning, profile):
+	profile["kerning"] = len(kerning)
+
+# ---------------
+# Profile: Groups
+# ---------------
+
+def profileGroups(groups, profile):
+	for group in groups.values():
+		count = len(group)
+		if not count:
+			continue
+		if count not in profile["groups"]:
+			profile["groups"][count] = 0
+		profile["groups"][count] += 1
+
+# -----------------
+# Profile: Features
+# -----------------
+
+def profileFeatures(text, profile):
+	if text is not None:
+		profile["features"] = len(text)
+
+# ------------------
+# Profile: Font Info
+# ------------------
+
+def profileFontInfo(info, profile):
+	pass
+
+# -----------------
+# Profile to String
+# -----------------
+
+def profileToString(profile):
+	lines = [
+		"> %s" % profile["fingerprint"],
+		"source format: %s" % profile["sourceFormat"],
+		"output environment: %s" % profile["outputEnvironment"],
+		"fingerprint: %s" % profile["fingerprint"],
+		"layers: %d" % len(profile["glyphs"]),
+		"glyphs: %d" % sum(profile["glyphs"]),
+		"kerning pairs: %d" % profile["kerning"]
+	]
+	for contourCount, occurance in reversed(sorted(profile["contourOccurance"].items())):
+		lines.append(
+			"glyphs with %d contours: %d" % (contourCount, occurance)
+		)
+	for pointCount, occurance in reversed(sorted(profile["pointOccurance"].items())):
+		lines.append(
+			"contours with %d points: %d" % (pointCount, occurance)
+		)
+	for pointType in ("moveTo", "lineTo", "curveTo", "qCurveTo"):
+		lines.append(
+			"%s points: %s" % (pointType, profile["pointTypes"][pointType])
+		)
+	lines.append("<")
+	return "\n".join(lines)
+
+# ----
+# Main
+# ----
+
+def dumpProfilesForFonts(paths):
+	for path in paths:
+		profile = profileFont(path)
+		print profileToString(profile)
+
+def gatherFontPaths(paths):
+	found = []
+	for path in paths:
+		if os.path.isdir(path):
+			for fileName in os.listdir(path):
+				if fileName.startswith("."):
+					continue
+				p = os.path.join(path, fileName)
+				found += gatherFontPaths([p])
+		else:
+			if isFontPath(path):
+				found.append(path)
+	return found
+
+def isFontPath(path):
+	fileName = os.path.basename(path)
+	suffix = os.path.splitext(fileName)[-1].lower()
+	return suffix in (".otf", ".ttf", ".ufo")
+
+# ------------
+# Command Line
+# ------------
 
 usage = "%prog [options] fontpath1 fontpath2 fontdirectory"
 
